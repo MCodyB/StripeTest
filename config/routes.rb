aaba1700fb6b00a3e05bf8f37437c81cead9027e
MCodyB <mcodyb@gmail.com>
2016-02-22 20:33:29 -0500

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   root to: "users#index"
 
   devise_for :users
+
+  resources :users, only: [:index, :show]
 
   resources :charges
 end
